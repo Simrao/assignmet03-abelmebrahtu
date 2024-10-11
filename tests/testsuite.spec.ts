24989e685c61ccb98139cfef7cef06c7e84174ee
Abel Mebrahtu <Abel.Meb1@gmail.com>
2024-10-11 23:03:23 +0200

    frontend test 01

--- tests/testsuite.spec.ts
+++ tests/testsuite.spec.ts
@@ -1,8 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Backend tests', async ()=> {
-  test('Get all posts',  async ({ request }) => {
-    const response = await request.get('http://localhost:3000/posts');
-    expect(response.ok()).toBeTruthy();
+test.describe('Frontend tests', () => {
+  test('Test01 - Logga in', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+    await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
   });
-});
+})
+
