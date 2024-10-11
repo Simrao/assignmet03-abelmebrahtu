c55bad4d4c9f371e7ee897832b9280c3a84ff863
Abel Mebrahtu <Abel.Meb1@gmail.com>
2024-10-11 23:12:13 +0200

    frontend test 02

--- tests/testsuite.spec.ts
+++ tests/testsuite.spec.ts
@@ -10,3 +10,20 @@ test.describe('Frontend tests', () => {
   });
 })
 
+test('Test02 - Skapa faktura', async ({ page }) => {
+
+  await page.goto('http://localhost:3000');
+  await page.locator('input[type="text"]').fill(`${process.env.TEST_USERNAME}`);
+  await page.locator('input[type="password"]').fill(`${process.env.TEST_PASSWORD}`);
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+
+  await page.locator('#app > div > div > div:nth-child(3) > a').click();
+  await page.locator('#app > div > h2 > a').click();
+
+  await page.fill('input[type="number"]', '3500');
+
+  await page.locator('a.btn.blue').click();
+
+  await expect(page.locator('text=Bills')).toBeVisible();
+});
